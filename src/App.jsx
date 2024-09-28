776f737c88e524ee8e46cc25ca68858ba698f7d8
aftab <shaikaftab>
2024-09-28 17:29:41 +0530

    made a siple counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // here one is like variable name and the other is the function
+  // the refernce what you what it to be done in the whole code so we 
+  // can just call it any where in the whole code instead of using js and updating it in the whole code
+  let [Counter,setCounter]=useState(0)
+  //  let Counter=5;
+   const addvalue=()=>{
+    //  Counter++;
+    setCounter(Counter+1)
+         
+   }
+   const removeValue=()=>{
+    setCounter(Counter-1)
+   }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>chai aur react </h1>
+      <h2>Counter value:{Counter}</h2>
+      <button onClick={addvalue}>Add value{Counter}</button>
+      <br>
+      </br>
+      <button onClick={removeValue}> Remove value{Counter}</button>
+      <p>footer: {Counter}</p>
     </>
   )
 }
